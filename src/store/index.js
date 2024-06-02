158ec7898f1290d143d1de87c3312672e347568f
小明 <2431165604@qq.com>
2024-06-02 10:43:58 +0800

    feat: 新增loading的Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import getters from "./getters";
+import getters from "@/store/getters";
+import loading from "@/store/module/loading";
 
 Vue.use(Vuex);
 
 let store = new Vuex.Store({
-    modules: {
-
-    },
-    getters,
+  modules: {
+    loading,
+  },
+  getters,
 })
 
 export default store;
