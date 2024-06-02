6f797679b9253cb6799316c3b9b30c154d3d871b
小明 <2431165604@qq.com>
2024-06-02 22:08:11 +0800

    feat: 新增moment 时间组件

--- src/main.js
+++ src/main.js
@@ -13,11 +13,17 @@ import 'lib-flexible';
 
 // vConsole
 import VConsole from "vconsole";
-
 if(process.env.NODE_ENV === "development") {
   const vConsole = new VConsole();
   console.log(vConsole, 'e')
 }
+
+// 时间控件
+import moment from "moment"
+Vue.prototype.$moment = moment
+
+// 设置全局事件总线
+
 
 Vue.config.productionTip = false
 
