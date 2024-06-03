8abe006130097f757c34d710657b7a9d05e1182d
小明 <2431165604@qq.com>
2024-06-04 00:40:53 +0800

    fix: 修改表单组件

--- src/components/Form/index.vue
+++ src/components/Form/index.vue
@@ -7,6 +7,20 @@
 <script>
 export default {
   name: 'CustomForm',
+  prop: {
+    // 表单配置
+    config: {
+      type: Array,
+      require: true,
+      default: () => []
+    },
+    // 表单数据
+    formData: {
+      type: Object,
+      require: true,
+      default: () => {}
+    }
+  },
   data() {
     return {
 
