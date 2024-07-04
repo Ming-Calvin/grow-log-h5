def0ee24a4748ff94aadfb7e337dee25cf5c8999
小明 <os.zhengweijie>
2024-07-04 18:06:40 +0800

    修改样式

--- src/view/layout/whiteNoise.vue
+++ src/view/layout/whiteNoise.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="whiteNoise">
+    <div>
+      Mindfulness
+      Meditation Intro
+    </div>
+
+    <div>
+      05:55
+    </div>
+
+
     <div class="currentCourse">
       <div class="icon"></div>
-
       <div>
-        <div> Current Course </div>
-        <div> noiseName </div>
+        <div class="course"> Current Course </div>
+        <div class="noiseName"> noiseName </div>
         <div class="time"> 15min </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -43,18 +53,36 @@ export default {
 <style scoped lang="scss">
 .whiteNoise {
   background-image: url('@/assets/picture/white-noise-background.png');
+  background-color: #9bb068;
   background-repeat: repeat;
   background-position: center;
   background-size: 100% auto;
   height: 100vh;
+  padding: 20% 0;
   margin: 0;
+  @include flex-j-a(space-around, center);
+  flex-direction: column;
 }
 
 .currentCourse {
   width: 90%;
-  margin: auto;
   border-radius: 10px;
   background-color: #fff;
   padding: 10px;
+
+  .course {
+    font-size: 10px;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+  }
+
+  .noiseName {
+    font-size: 18px;
+  }
+
+  .time {
+    //font-family: Urbanist;
+    font-size: 14px;
+  }
 }
 </style>
